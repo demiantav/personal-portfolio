2f1940b5e70ae362abefbe399ecd220a6c6f98d5
Demian Tavolaro <demiantavolaro6@gmail.com>
2024-03-28 20:01:54 -0300

    Se añadio seccion hero

--- src/js/textAnimate.js
+++ src/js/textAnimate.js
@@ -1,6 +1,7 @@
 export const pageLoad = () => {
-  const $titleChars = document.querySelectorAll('span');
-  const $dot = document.querySelector('.dot');
+  const $titleChars = document.querySelectorAll('.char');
+  const $logo = document.querySelector('.demi-logo');
+  const $hamb = document.querySelectorAll('.hamb');
   const $containerblue = document.querySelector('.container-initial-animation');
   const tl = gsap.timeline({ delay: 1 });
 
@@ -9,13 +10,23 @@ export const pageLoad = () => {
     stagger: 0.05,
     ease: 'back.out',
     duration: '.65',
-  }).to(
-    $containerblue,
-    {
-      clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-      duration: 1,
-      ease: 'power3.inOut',
-    },
-    '<+=1.8'
-  );
+  })
+    .to(
+      $containerblue,
+      {
+        clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+        duration: 1,
+        ease: 'power3.inOut',
+      },
+      '<+=1.8'
+    )
+    .from(
+      [$hamb, $logo],
+      {
+        yPercent: 200,
+        ease: 'power3.inOut',
+        duration: 1.5,
+      },
+      '<'
+    );
 };
