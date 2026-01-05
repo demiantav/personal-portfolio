f933577b01d1bba799a05fec2d83edac3f17b486
Demián Ariel Tavolaro <demiantavolaro6@gmail.com>
2026-01-05 02:31:14 +0100

    Develop (#28)
    * Feature/animation hero (#25)
    
    * feat: apply view transitions to nav links
    
    * fix: spelling mistake
    
    * feat: add favicon and fix padding on nav links
    
    * feat: improve view transitions
    
    * Feature/footer clock (#27)
    
    * feat: first clock logical implementation
    
    * feat: style to clock, animation blinking in colon
    
    * feat: better clock performance

---
+++ src/js/time.js
@@ -0,0 +1,22 @@
+const setClock = () => {
+  const d = document;
+  const $hour = d.querySelector('.hour');
+  const $colon = d.querySelector('.colon');
+  const $minutes = d.querySelector('.minutes');
+
+  const updateTime = () => {
+    const now = new Date();
+    const options = { hour: '2-digit', minute: '2-digit', hour12: false };
+    const timeString = now.toLocaleTimeString('it-IT', options);
+
+    const [hours, minutes] = timeString.split(':');
+
+    $hour.textContent = hours;
+    $minutes.textContent = minutes;
+  };
+
+  updateTime();
+  setInterval(updateTime, 1000);
+};
+
+export default setClock;
