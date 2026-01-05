a21395ce6b5f988e1efe7a941ebb136f895cc765
Demián Ariel Tavolaro <demiantavolaro6@gmail.com>
2026-01-05 01:45:03 +0100

    Feature/footer clock (#27)
    * feat: first clock logical implementation
    
    * feat: style to clock, animation blinking in colon
    
    * feat: better clock performance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { animateMenu } from './js/menuAnimation.js';
 import { pageLoad, animateSectionText } from './js/textAnimate.js';
 import { Waves } from './js/waves.js';
+import setClock from './js/time.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   pageLoad();
   animateMenu();
   animateSectionText();
+  setClock();
 });
