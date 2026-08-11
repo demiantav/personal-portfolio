1001595a88df59aa8c4e79d7b568988017f6e85a
Demian Tavolaro <demiantavolaro6@gmail.com>
2026-08-11 13:15:28 +0200

    fix: update on script

--- src/js/fixHeroBug.js
+++ src/js/fixHeroBug.js
@@ -4,6 +4,7 @@ export const fixHeroBug = () => {
   const aboutSection = document.querySelector('.main__about-me-section');
   const main = document.querySelector('#hero');
 
+  const headerHeight = header.offsetHeight + 5;
   let isOutside = false;
 
   window.addEventListener(
@@ -11,14 +12,11 @@ export const fixHeroBug = () => {
     () => {
       const rect = aboutSection.getBoundingClientRect();
 
-      // 1. EXTRAER: Al llegar a 'About me', saca el header al inicio de <main>
       if (rect.top <= 0 && !isOutside) {
         main.prepend(header);
         header.classList.add('is-sticky');
         isOutside = true;
-      }
-      // 2. RETORNAR: Al subir de nuevo, lo devuelve al final de la section hero
-      else if (rect.top > 0 && isOutside) {
+      } else if (rect.top > headerHeight && isOutside) {
         heroSection.append(header);
         header.classList.remove('is-sticky');
         isOutside = false;
