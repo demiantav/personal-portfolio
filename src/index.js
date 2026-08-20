5f648eab4db332091b0b0c2ccd7014f43b778d72
Demian Tavolaro <demiantavolaro6@gmail.com>
2026-08-20 13:23:14 +0200

    fixed: header scroll animation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import { animateMenu } from './js/menuAnimation.js';
 import { pageLoad, animateSectionText } from './js/textAnimate.js';
 import setClock from './js/time.js';
 import horizontalAnimation from './js/horizontalAnimation.js';
-import { fixHeroBug } from './js/fixHeroBug.js';
 import { animateReveals } from './js/reveal-animations.js';
 import { parallaxAnimations } from './js/parallax-animations.js';
 import { initHeroZoomTransition } from './js/hero-zoom-transition.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  //fixHeroBug();
   pageLoad();
   animateMenu();
   animateSectionText();
